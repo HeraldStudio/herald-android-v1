ad9c2d244fd147c4de6bae6aee4d396fa02a43b7
rikumi <riku.mi@outlook.com>
2016-03-08 20:40:03 +0800

    修正Android5.0以上标题栏阴影的问题

--- app/src/main/java/cn/seu/herald_android/BaseAppCompatActivity.java
+++ app/src/main/java/cn/seu/herald_android/BaseAppCompatActivity.java
@@ -72,11 +72,13 @@ public class BaseAppCompatActivity extends AppCompatActivity {
                 ||Build.VERSION.SDK_INT<Build.VERSION_CODES.LOLLIPOP) {
             // 设置状态栏透明
             activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            /*
             // 生成一个状态栏大小的矩形
             View statusView = createStatusView(activity, color);
             // 添加 statusView 到布局中
             ViewGroup decorView = (ViewGroup) activity.getWindow().getDecorView();
             decorView.addView(statusView);
+            */
             // 设置根布局的参数
             ViewGroup rootView = (ViewGroup) ((ViewGroup) activity.findViewById(android.R.id.content)).getChildAt(0);
             rootView.setFitsSystemWindows(true);
