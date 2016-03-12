9b8dba4e23561bd7fd1b3628267f6b870d165d09
YongdongHe <heyongdonghe@qq.com>
2016-03-12 22:08:52 +0800

    关于我们

---
+++ app/src/main/java/cn/seu/herald_android/mod_communicate/AboutusActivity.java
@@ -0,0 +1,32 @@
+package cn.seu.herald_android.mod_communicate;
+
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
+
+import cn.seu.herald_android.BaseAppCompatActivity;
+import cn.seu.herald_android.R;
+
+public class AboutusActivity extends BaseAppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutus);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_keyboard_backspace_24dp);
+        toolbar.setNavigationOnClickListener(v -> {
+            onBackPressed();
+            finish();
+        });
+
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.toolbar_layout);
+        collapsingToolbarLayout.setExpandedTitleGravity(Gravity.RIGHT);
+    }
+}
