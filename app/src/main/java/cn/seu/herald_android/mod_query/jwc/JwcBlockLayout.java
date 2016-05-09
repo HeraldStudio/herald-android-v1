c4c3ae8fe196b32bcc25c66886163d4eb74e2df3
YongdongHe <heyongdonghe@qq.com>
2016-05-10 01:58:48 +0800

    回滚教务处卡片标题设置

--- app/src/main/java/cn/seu/herald_android/mod_query/jwc/JwcBlockLayout.java
+++ app/src/main/java/cn/seu/herald_android/mod_query/jwc/JwcBlockLayout.java
@@ -23,9 +23,7 @@ public class JwcBlockLayout extends LinearLayout {
 
         title.setTextColor(ContextCompat.getColor(getContext(), R.color.colorJwcprimary));
         title.setMinLines(1);
-        title.setMaxLines(3);
-        //一行最多14个字符
-        title.setMaxEms(14);
+        title.setMaxLines(2);
 
         title.setText(item.title);
         content.setText("发布时间：" + item.date);
