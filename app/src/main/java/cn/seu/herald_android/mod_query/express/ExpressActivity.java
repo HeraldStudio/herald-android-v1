e28e4f8724273d33043fe5be22e6932360dc1513
corvo <corvofeng@gmail.com>
2016-07-30 14:23:07 +0800

    短信查询与界面设计基本完成

--- app/src/main/java/cn/seu/herald_android/mod_query/express/ExpressActivity.java
+++ app/src/main/java/cn/seu/herald_android/mod_query/express/ExpressActivity.java
@@ -1,7 +1,22 @@
 package cn.seu.herald_android.mod_query.express;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 import cn.seu.herald_android.R;
 import cn.seu.herald_android.app_framework.BaseActivity;
@@ -9,17 +24,45 @@ import cn.seu.herald_android.app_framework.BaseActivity;
 /**
  * Created by corvo on 7/28/16.
  */
-public class ExpressActivity extends BaseActivity {
+public class ExpressActivity extends BaseActivity implements AdapterView.OnItemSelectedListener{
+
+    private Spinner mSpiner;
+    ArrayList<String> data = new ArrayList<String>(Arrays.asList("梅九", "桃三四"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mod_que_express);
+        mSpiner = (Spinner)findViewById(R.id.express_spinner_dest);
+
+        ArrayAdapter<String> dataAdapter =
+                new ArrayAdapter<String>(this, R.layout.mod_que_express__dest_item, data);
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpiner.setAdapter(dataAdapter);
+        mSpiner.setOnItemSelectedListener(this);
+
+        Uri uri = Uri.parse("content://sms/inbox");
+        SmsContent smsContent = new SmsContent(this, uri);
+        List<SmsInfo> list = smsContent.getInfos();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        //getMenuInflater().inflate(R.menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //Log.d("ExpressActivity", String.valueOf(view.getId()));
+        Spinner spinner = (Spinner) parent;
+        if (spinner.getId() == R.id.express_spinner_dest) {
+            Log.d("ExpressActivity", data.get(position));
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
---
+++ app/src/main/java/cn/seu/herald_android/mod_query/express/SmsContent.java
@@ -0,0 +1,92 @@
+package cn.seu.herald_android.mod_query.express;
+
+import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.util.Log;
+
+import java.sql.Time;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by corvo on 7/30/16.
+ */
+public class SmsContent {
+    private final String TAG = "SmsContent";
+
+    private Context mContext;
+    private Uri mUri;
+
+    List<SmsInfo> infos;
+
+    public SmsContent(Context context, Uri uri) {
+        infos = new ArrayList<SmsInfo>();
+        this.mContext = context;
+        this.mUri = uri;
+    }
+
+    // 获取
+    public List<SmsInfo> getInfos() {
+        String[] projection = new String[] {"_id", "address", "person", "body", "date", "type"};
+
+        Log.d(TAG, "In Func getInfos");
+        Cursor cursor = null;
+        try {
+
+            /**
+             * 获取时间, 过滤短信, 默认只读入24小时以内的短信,
+             * 如若24小时内未取快递, 那么短信一定已经失效, 快递已被移动
+             */
+            Date dt = new Date();
+            long cur = dt.getTime();                            // 当前时
+            long last = cur - TimeUnit.HOURS.toMillis(24);      // 减去24小时
+
+            Log.d(TAG, "before 24 hours, time is " + String.valueOf(last));
+
+            String filter = "date > " + String.valueOf(last);   // 查询中的where语句, date > 1469839964323
+
+            cursor = mContext.getContentResolver().query(mUri, projection, filter, null, "date desc");
+
+            if (cursor == null) {
+                Log.d(TAG, "cursor is null. uri:" + mUri);
+            }
+
+            int nameColumn = cursor.getColumnIndex("person");
+            int phoneNumberColumn = cursor.getColumnIndex("address");
+            int smsbodyColumn = cursor.getColumnIndex("body");
+            int dateColumn = cursor.getColumnIndex("date");
+            int typeColumn = cursor.getColumnIndex("type");
+
+            /**
+             *  读取信息存入数组
+             */
+            while (cursor.moveToNext()) {
+                SmsInfo smsInfo = new SmsInfo();
+                smsInfo.setName(cursor.getString(nameColumn));
+                smsInfo.setDate(cursor.getString(dateColumn));
+                smsInfo.setPhoneNumber(cursor.getString(phoneNumberColumn));
+                smsInfo.setType(cursor.getString(typeColumn));
+                smsInfo.setSmsbody(cursor.getString(smsbodyColumn));
+
+                Log.d(TAG, cursor.getString(phoneNumberColumn));
+                Log.d(TAG, cursor.getString(smsbodyColumn));
+                Log.d(TAG, cursor.getString(dateColumn));
+
+                infos.add(smsInfo);
+            }
+
+        } catch (Exception e) {
+            Log.d(TAG, "Failed in query");
+            e.printStackTrace();
+        } finally {
+            cursor.close();
+        }
+
+        return infos;
+    }
+}
